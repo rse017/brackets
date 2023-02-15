87999e63b7f4b81bc148852b068f29c4a5c30e34
Sergei R <rse017@gmail.com>
2023-02-15 21:39:09 +0300

    feat: test solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const val = bracketsConfig.reduce((a, b) => {
+    a[b[0]] = b[1];
+    return a;
+  }, {})
+
+  const arr = [];
+  for (let i = 0; i < str.length; i++) {
+    const c = arr[arr.length - 1];
+    const d = str[i];
+    if (val[c] == d) {
+      arr.pop();
+      continue;
+    } if (val[d]) {
+      arr.push(d);
+      continue;
+    } if (arr.length == 0) {
+      return false;
+    }
+  }
+  return arr.length == 0
 }
